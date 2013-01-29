13e891b151352288764b34850a7be88c9a84cff8
Nirvana Tikku <ntikku@gmail.com>
2013-01-28 22:50:20 -0500

    Fix: spacing issues, user score instead of acceptance rate, radio button and associated label w/ click, update question view look: title is the toggle now, and score instead of answer_count, look and feel, question click issues -- converting xml to html content now - bad nodetype

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-The Stack-Ed website.
+# The Stack-Ed website.
 
-* Supply an application name in app.yaml
-* Doesn't have a GA ID.
-* StackOverflow key needed in main.js
-* I don't believe I can include the pictos webfont due to licensing reasons. The icons won't look correct (they'll be chars/numbers).
+	* Supply an application name in app.yaml
+	* Doesn't have a GA ID.
+	* StackOverflow key needed in main.js
+	* I don't believe I can include the pictos webfont due to licensing reasons. (The icons won't look correct - they'll be chars/numbers)
--- js/app.js
+++ js/app.js
@@ -1,78 +1,84 @@
 define(['backbone'],function(){
 
-	var App = Backbone.Model.extend({
-		defaults: { 
-			user: undefined,
-			cache: { }
-		},
-		initialize:function(incoming){
-			this.view = new AppView({
-				el: document.body,
-				model: this
-			});
-		}
-	});
+    var App = Backbone.Model.extend({
+        defaults: { 
+            user: undefined,
+            cache: { }
+        },
+        initialize:function(incoming){
+            this.view = new AppView({
+                el: document.body,
+                model: this
+            });
+        }
+    });
 
-	var AppView = Backbone.View.extend({
-		$filters: null,
-		$tags: null,
-		setSearchFilter: function setSearchFilter(tag, user){
-			var tmpl = _.template("&ndash; <div class='label'><%= lbl %></div>");
-			var html = tmpl({lbl:tag});
-			if( user ) { 
-				html += " " + tmpl({lbl:user});
-			}
-			this.$filters.html(html);
-		},
-		initialize:function(){
-			_.bindAll(this,'domReady');
-			// 
-			$(document).ready(this.domReady);
-		},
-		addTag: function(tagEl){
-			this.$tags.append(tagEl);
-		},
-		addQuestion: function(qEl){
-			this.$questions.append(qEl);
-		},
-		clearQuestions: function(){
-			this.$questions.html("");
-		},
-		addAnswerer: function(aEl){
-			this.$answerers.append(aEl);
-		},
-		clearAnswerers: function(){
-			this.$answerers.html("");
-		},
-		showAnswerersPanel: function(){
-			this.$answerersContainer.show();
-		},
-		showQuestionsPanel: function(){
-			this.$questionsContainer.show();
-		},
-		showTagsPanel: function(){
-			this.$tagsContainer.show();
-		},
-		removeIntroMessage: function(){
-			this.$el.find(".intro-message").remove();
-		},
-		showNavbar:function(){
-			this.$navbar.css("opacity","1");
-		},
-		domReady: function(){
-			this.$filters = this.$el.find(".js-filter-criteria");
-			this.$tags = this.$el.find(".js-tags");
-			this.$questions = this.$el.find(".js-questions");
-			this.$answerers = this.$el.find(".js-top-answerers");
-			this.$answerersContainer = this.$el.find(".js-answerers-container");
-			this.$questionsContainer = this.$el.find(".js-questions-container");
-			this.$tagsContainer = this.$el.find(".js-tags-container");
-			this.$navbar = this.$el.find(".js-navbar");
-			this.$userNumStarred = this.$el.find(".js-stacked-user-num-starred");
-			return this;
-		}
-	});
+    var AppView = Backbone.View.extend({
+        $filters: null,
+        $tags: null,
+        setSearchFilter: function setSearchFilter(tag, user){
+            var tmpl = _.template("&ndash; <div class='label'><%= lbl %></div>");
+            var html = tmpl({lbl:tag});
+            if( user ) { 
+                html += " " + tmpl({lbl:user});
+            }
+            this.$filters.html(html);
+        },
+        initialize:function(){
+            _.bindAll(this,'domReady');
+            // 
+            $(document).ready(this.domReady);
+        },
+        addTag: function(tagEl){
+            this.$tags.append(tagEl);
+        },
+        addQuestion: function(qEl){
+            this.$questions.append(qEl);
+        },
+        clearQuestions: function(){
+            this.$questions.html("");
+        },
+        addAnswerer: function(aEl){
+            this.$answerers.append(aEl);
+        },
+        clearAnswerers: function(){
+            this.$answerers.html("");
+        },
+        showAnswerersPanel: function(){
+            this.$answerersContainer.show();
+        },
+        hideAnswerersPanel: function(){
+            this.$answerersContainer.hide();
+        },
+        showQuestionsPanel: function(){
+            this.$questionsContainer.show();
+        },
+        hideQuestionsPanel: function(){
+            this.$questionsContainer.hide();
+        },
+        showTagsPanel: function(){
+            this.$tagsContainer.show();
+        },
+        removeIntroMessage: function(){
+            this.$el.find(".intro-message").remove();
+        },
+        showNavbar:function(){
+            this.$navbar.css("opacity","1");
+        },
+        domReady: function(){
+            this.$filters = this.$el.find(".js-filter-criteria");
+            this.$tags = this.$el.find(".js-tags");
+            this.$questions = this.$el.find(".js-questions");
+            this.$answerers = this.$el.find(".js-top-answerers");
+            this.$answerersContainer = this.$el.find(".js-answerers-container");
+            this.$questionsContainer = this.$el.find(".js-questions-container");
+            this.$tagsContainer = this.$el.find(".js-tags-container");
+            this.$navbar = this.$el.find(".js-navbar");
+            this.$userNumStarred = this.$el.find(".js-stacked-user-num-starred");
+            return this;
+        }
+    });
 
-	return new App();
+    return new App();
 
 });
